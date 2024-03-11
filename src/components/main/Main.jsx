b5566efb2d38d895bbc2f8d2c7750a133a312924
mohitmakda <111580134+mohitmakda@users.noreply.github.com>
2024-03-12 01:46:04 +0530

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
 	return (
 		<div className="main">
 			<div className="nav">
-				<p>Gemini</p>
+				<p>Vistaar</p>
 				<img src={assets.user} alt="" />
 			</div>
 			<div className="main-container">
